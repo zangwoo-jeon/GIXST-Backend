985e7c845bec3bea80582c22d73277623adb0ee0
zangwoo Jeon <create7827@naver.com>
2025-12-27 03:56:25 +0900

    fix : 로그인 리다이렉트 수정

--- src/main/java/com/AISA/AISA/global/config/SecurityConfig.java
+++ src/main/java/com/AISA/AISA/global/config/SecurityConfig.java
@@ -66,6 +66,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedOrigin("https://gixst.vercel.app");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);

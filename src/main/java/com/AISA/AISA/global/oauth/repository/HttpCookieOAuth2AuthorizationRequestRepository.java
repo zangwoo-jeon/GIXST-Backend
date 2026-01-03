6c6d061023a36b21d0df0cf97302a7857259c1b8
zangwoo Jeon <create7827@naver.com>
2026-01-03 15:58:42 +0900

    fix : 구글 로그인 버그 로그 추가

--- src/main/java/com/AISA/AISA/global/oauth/repository/HttpCookieOAuth2AuthorizationRequestRepository.java
+++ src/main/java/com/AISA/AISA/global/oauth/repository/HttpCookieOAuth2AuthorizationRequestRepository.java
@@ -18,20 +18,41 @@ public class HttpCookieOAuth2AuthorizationRequestRepository
 
     @Override
     public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
-        return CookieUtils.getCookie(request, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME)
-                .map(cookie -> CookieUtils.deserialize(cookie, OAuth2AuthorizationRequest.class))
+        OAuth2AuthorizationRequest authRequest = CookieUtils
+                .getCookie(request, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME)
+                .map(cookie -> {
+                    // log.debug("Found OAuth2 Cookie: {}", cookie.getValue()); // too verbose for
+                    // base64
+                    log.info("Found OAuth2 Cookie in request");
+                    return CookieUtils.deserialize(cookie, OAuth2AuthorizationRequest.class);
+                })
                 .orElse(null);
+
+        if (authRequest == null) {
+            log.warn("No OAuth2 Authorization Request Cookie found.");
+            // Debugging: Print all cookies
+            if (request.getCookies() != null) {
+                for (jakarta.servlet.http.Cookie c : request.getCookies()) {
+                    log.info("Available Cookie: name={}, domain={}, path={}", c.getName(), c.getDomain(), c.getPath());
+                }
+            } else {
+                log.info("No cookies present in request.");
+            }
+        }
+        return authRequest;
     }
 
     @Override
     public void saveAuthorizationRequest(OAuth2AuthorizationRequest authorizationRequest, HttpServletRequest request,
             HttpServletResponse response) {
         if (authorizationRequest == null) {
+            log.info("Deleting OAuth2 Cookies because authorizationRequest is null");
             CookieUtils.deleteCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
             CookieUtils.deleteCookie(request, response, REDIRECT_URI_PARAM_COOKIE_NAME);
             return;
         }
 
+        log.info("Saving OAuth2 Authorization Request Cookie. redirect_uri: {}", authorizationRequest.getRedirectUri());
         CookieUtils.addCookie(response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME,
                 CookieUtils.serialize(authorizationRequest), cookieExpireSeconds);
         String redirectUriAfterLogin = request.getParameter(REDIRECT_URI_PARAM_COOKIE_NAME);
@@ -43,6 +64,7 @@ public class HttpCookieOAuth2AuthorizationRequestRepository
     @Override
     public OAuth2AuthorizationRequest removeAuthorizationRequest(HttpServletRequest request,
             HttpServletResponse response) {
+        log.info("removeAuthorizationRequest called");
         return this.loadAuthorizationRequest(request);
     }
 
